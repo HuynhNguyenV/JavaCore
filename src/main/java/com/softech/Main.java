ae9503f2ef624c927992e6427f59d3d5ea5e9729
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-04-09 16:09:03 +0700

    variables

--- src/main/java/com/softech/Main.java
+++ src/main/java/com/softech/Main.java
@@ -3,5 +3,15 @@ package com.softech;
 public class Main {
     public static void main(String[] args){
         System.out.println("hello world");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
